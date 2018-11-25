ce6a5435db79f96e606f4469bf978f2dfed0b4b9
Nikola Totev <nikola.r.totev@gmail.com>
2018-11-25 13:43:08 +0200

    Added Project

---
+++ Beauty/Beauty/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Beauty
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_SetSave = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.drawPhl = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 64);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1130, 608);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.btn_SetSave);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.drawPhl);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1130, 64);
+            this.panel2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(737, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // btn_SetSave
+            // 
+            this.btn_SetSave.Location = new System.Drawing.Point(643, 18);
+            this.btn_SetSave.Name = "btn_SetSave";
+            this.btn_SetSave.Size = new System.Drawing.Size(88, 28);
+            this.btn_SetSave.TabIndex = 4;
+            this.btn_SetSave.Text = "Save Location";
+            this.btn_SetSave.UseVisualStyleBackColor = true;
+            this.btn_SetSave.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(526, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 45);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Tree";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(182, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Tree";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(97, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Circle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // drawPhl
+            // 
+            this.drawPhl.Location = new System.Drawing.Point(12, 12);
+            this.drawPhl.Name = "drawPhl";
+            this.drawPhl.Size = new System.Drawing.Size(79, 45);
+            this.drawPhl.TabIndex = 0;
+            this.drawPhl.Text = "Spiral";
+            this.drawPhl.UseVisualStyleBackColor = true;
+            this.drawPhl.Click += new System.EventHandler(this.drawPhl_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 672);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button drawPhl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btn_SetSave;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Beauty/Beauty/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Beauty
+{
+    public partial class Form1 : Form
+    {
+        // Spiral variables
+        private float amount;
+        private float n = 0;
+        private float c = 4;
+        private float angle;
+        private float r;
+        private float size = 2;
+        private float x;
+        private float y;
+
+
+        //Fractal Variables
+
+        //Circle Variables
+        private float posX;
+        private float posY;
+        private float radius = 555;
+
+        // Squares 
+        private float sO = 100;
+        private int colGen;
+        private int pictureNumber;
+        private int sessionNumber;
+        private int itterations = 1;
+        private int prev = 0;
+        private Random newRandom = new Random();
+        private Color col1 = Color.AliceBlue;
+        private Color col2 = Color.CornflowerBlue;
+        private Color[] colors;
+        private Graphics g = null;
+        private Bitmap bmp;
+        private SolidBrush sBrush = new SolidBrush(Color.White);
+        private Pen myPen = new Pen(Color.White);
+        private string saveDir;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            g = panel1.CreateGraphics();
+
+            posX = panel1.Width;
+            posY = panel1.Height;
+
+            bmp = new Bitmap(panel1.Width, panel1.Height);
+
+            // col1 = Color.FromArgb(newRandom.Next(0, 255), newRandom.Next(0, 255), newRandom.Next(0, 255));
+            // col2 = Color.FromArgb(newRandom.Next(0, 255), newRandom.Next(0, 255), newRandom.Next(0, 255));
+            colors = new Color[] { col1, col2 };
+            pictureNumber = Properties.Settings.Default.pictureNumber;
+            sessionNumber = Properties.Settings.Default.sessionNumber;
+
+
+
+        }
+
+        private void drawPhl_Click(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(panel1.Width, panel1.Height);
+
+            //col1 = Color.FromArgb(newRandom.Next(0, 255), newRandom.Next(0, 255), newRandom.Next(0, 255));
+            // col2 = Color.FromArgb(newRandom.Next(0, 255), newRandom.Next(0, 255), newRandom.Next(0, 255));
+            colors = new Color[] { col1, col2 };
+
+            panel1.Refresh();
+            g = panel1.CreateGraphics();
+            r = 0;
+            x = 0;
+            y = 0;
+            angle = 0;
+            n = 0;
+            size = 3;
+
+            for (int i = 0; i < 15000; i++)
+            {
+                colGen = newRandom.Next(0, 1000);
+
+                if (colGen % 2 == 0)
+                {
+                    sBrush.Color = colors[0];
+                }
+
+                if (colGen % 2 != 0)
+                {
+                    sBrush.Color = colors[1];
+                }
+
+                // Color c1 = Color.FromArgb(newRandom.Next(0, 255), newRandom.Next(0, 255), newRandom.Next(0, 255));
+                //sBrush.Color = c1;
+
+                angle = n * 137.3f * .017453292519f;
+                r = (float)(c * Math.Sqrt(n));
+                x = (float)(r * Math.Cos(angle) + panel1.Width / 2);
+                y = (float)(r * Math.Sin(angle) + panel1.Height / 2);
+
+
+                g.FillEllipse(sBrush, x, y, size, size);
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+                size += 0.0005f;
+                n++;
+            }
+
+            g = Graphics.FromImage(bmp);
+            Rectangle rect = panel1.RectangleToScreen(panel1.ClientRectangle);
+            g.CopyFromScreen(rect.Location, Point.Empty, panel1.Size);
+            string fileName = saveDir + string.Format("\\Fractal{0}.png", pictureNumber.ToString());
+            bmp.Save(fileName);
+            pictureNumber++;
+            Properties.Settings.Default.pictureNumber = this.pictureNumber;
+            Properties.Settings.Default.Save();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            itterations = 1;
+            SetDirectory();
+            //col1 = Color.FromArgb(newRandom.Next(0, 255), newRandom.Next(0, 255), newRandom.Next(0, 255));
+            // col2 = Color.FromArgb(newRandom.Next(0, 255), newRandom.Next(0, 255), newRandom.Next(0, 255));
+
+            colors = new Color[] { col1, col2 };
+
+
+            panel1.Refresh();
+            g = panel1.CreateGraphics();
+            posX = panel1.Width;
+            posY = panel1.Height;
+            DrawCircle(posX / 2, posY / 2, radius);
+        }
+
+        /// <summary>
+        /// Draws a circle fractal based on given parameters == Reccursive function ==
+        /// </summary>
+        /// <param name="pX"></param>
+        /// <param name="pY"></param>
+        /// <param name="d"></param>
+
+        #region fractals
+        private void DrawCircle(float pX, float pY, float d)
+        {
+            colGen = newRandom.Next(0, 1000);
+
+            if (colGen % 2 == 0)
+            {
+                myPen.Color = colors[0];
+                sBrush.Color = colors[0];
+            }
+
+
+
+            if (colGen % 2 != 0)
+            {
+                myPen.Color = colors[1];
+                sBrush.Color = colors[1];
+            }
+
+
+
+            g.DrawEllipse(myPen, pX - d / 2, pY - d / 2, d, d);
+           //g.FillEllipse(sBrush, pX - d / 2, pY - d / 2, d, d);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            if (d > 1)
+            {
+                DrawCircle(pX + d * 0.5f, pY, d * 0.5f);
+                DrawCircle(pX - d * 0.5f, pY, d * 0.5f);
+                // DrawCircle(pX, pY + d * 0.5f, d * 0.5f);
+                // DrawCircle(pX , pY - d * 0.5f, d * 0.5f);
+
+            }
+            itterations++;
+            if (itterations == 2048)
+            {
+
+                g = Graphics.FromImage(bmp);
+                Rectangle rect = panel1.RectangleToScreen(panel1.ClientRectangle);
+                g.CopyFromScreen(rect.Location, Point.Empty, panel1.Size);
+                string fileName = saveDir + string.Format("\\Fractal{0}.png", pictureNumber.ToString());
+                bmp.Save(fileName);
+                pictureNumber++;
+                Properties.Settings.Default.pictureNumber = this.pictureNumber;
+                Properties.Settings.Default.Save();
+            }
+
+
+        }
+
+        private void Squares(float pX1, float pY2, float s)
+        {
+            colGen = newRandom.Next(0, 1000);
+
+            if (colGen % 2 == 0)
+            {
+                myPen.Color = colors[0];
+                sBrush.Color = colors[0];
+            }
+
+
+            if (colGen % 2 != 0)
+            {
+                myPen.Color = colors[1];
+                sBrush.Color = colors[1];
+            }
+
+
+            //g.DrawRectangle(myPen, pX1 - s / 2, pY2 - s / 2, s, s);
+            g.FillEllipse(sBrush, pX1 - s / 2, pY2 - s / 2, s, s);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            if (s > 1)
+            {
+
+                Squares((float)(pX1 + s * 0.5f * Math.Cos(59)), (float)(pY2 * Math.Sin(30)), s * 0.5f);
+                Squares((float)(pX1 - s * 0.5f * Math.Sin(30)), (float)(pY2 * Math.Sin(30)), s * 0.5f);
+                Squares(pX1, pY2 + s * 0.5f, s * 0.5f);
+                //Squares(pX1, pY2 - s * 0.5f, s * 0.5f);
+            }
+            itterations++;
+
+            if (itterations > prev)
+            {
+                prev = itterations;
+
+            }
+            else if (prev == itterations)
+            {
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            itterations = 1;
+            SetDirectory();
+            //col1 = Color.FromArgb(newRandom.Next(0, 255), newRandom.Next(0, 255), newRandom.Next(0, 255));
+            //col2 = Color.FromArgb(newRandom.Next(0, 255), newRandom.Next(0, 255), newRandom.Next(0, 255));
+
+            colors = new Color[] { col1, col2 };
+
+            panel1.Refresh();
+            g = panel1.CreateGraphics();
+            Squares(panel1.Width / 2, panel1.Height / 2, sO);
+        }
+
+        #endregion
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SetDirectory();
+
+            for (int i = 0; i < 2; i++)
+            {
+                drawPhl_Click(sender, e);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            string test = saveFileDialog1.FileName;
+            textBox1.Text = test;
+        }
+
+        private void SetDirectory()
+        {
+            saveDir = @"D:\Desktop\Fractals\Session" + sessionNumber.ToString();
+            Directory.CreateDirectory(saveDir);
+            sessionNumber++;
+            Properties.Settings.Default.sessionNumber = this.sessionNumber;
+            Properties.Settings.Default.Save();
+            bmp = new Bitmap(panel1.Width, panel1.Height);
+        }
+    }
+}
